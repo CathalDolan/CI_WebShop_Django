16d409a622631d23ed0b2a2bcac59f9c142cc281
CathalDolan <cathal@gizagig.com>
2021-02-21 21:14:39 +0000

    Finished product size logic

--- CI_WebShop_Django/bag/contexts.py
+++ CI_WebShop_Django/bag/contexts.py
@@ -11,15 +11,30 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        """ Only want to run it if there are no sizes, so we check if the item_data
+        is an integer (indicating just a quantity) """
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        # If it has sizes, we iterate through a dict
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
@@ -41,4 +56,3 @@ def bag_contents(request):
     }
 
     return context
-    
--- CI_WebShop_Django/bag/views.py
+++ CI_WebShop_Django/bag/views.py
@@ -11,16 +11,34 @@ def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
-    # I thinkm, this allows the User to stay on the page they were on
+
+    # I think, this allows the User to stay on the page they were on
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
     # Check if there is a session called "bag" and if not, create one. It's a dict
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if size:
+        if item_id in list(bag.keys()):
+            # Checking if another item with same id and size already exists in the bag
+            if size in bag[item_id]['items_by_size'].keys():
+                # If Yes, increment the quantity for that size
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                # If not, just set the quantity to = the quantity. Applies to new sizes, same item
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            # If item not already in the bag, it's added in form of a doct with the size and quantity
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
-    
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
+
     # Over-write the current session or add the new one
     request.session['bag'] = bag
     return redirect(redirect_url)
