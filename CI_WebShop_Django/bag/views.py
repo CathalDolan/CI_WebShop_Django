7f4835ba742c6f45c2ea04b65c5ea3b6b5707827
CathalDolan <cathal@gizagig.com>
2021-02-21 18:38:24 +0000

    Added add to bag functionality

--- CI_WebShop_Django/bag/views.py
+++ CI_WebShop_Django/bag/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def view_bag(request):
+    """ Renders the bag contents page """
+
     return render(request, 'bag/bag.html')
+
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    # I thinkm, this allows the User to stay on the page they were on
+    redirect_url = request.POST.get('redirect_url')
+    # Check if there is a session called "bag" and if not, create one. It's a dict
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+    
+    # Over-write the current session or add the new one
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
